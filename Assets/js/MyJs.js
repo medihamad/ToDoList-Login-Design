184c0f8ac041e54891cfc4b33e7d2e2e1bbe5027
Medi hamad <kumarpatel692@gmail.com>
2018-10-13 12:53:32 +0300

    uploading ToDoList Login and signup Page

---
+++ Assets/js/MyJs.js
@@ -0,0 +1,12 @@
+function signing(){
+    var element1 = document.getElementById("forgot-pass");
+    element1.style.display = "block";
+    var element2 = document.getElementById("forgot-field");
+    element2.style.display = "block";
+}
+ function closing(){
+    var element3 = document.getElementById("forgot-pass");
+    element3.style.display = "none";
+    var element4 = document.getElementById("forgot-field");
+    element4.style.display = "none";
+} 
